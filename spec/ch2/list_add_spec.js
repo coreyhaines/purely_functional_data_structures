2620252145857c175587172878d71209a41a9090
coreyhaines <coreyhaines@gmail.com>
2010-05-20 16:08:46 +0200

    Multiple elements in first list in add

--- spec/ch2/list_add_spec.js
+++ spec/ch2/list_add_spec.js
@@ -22,4 +22,23 @@ var sys = require('sys'),
   }());
 }());
 
+(function() {
+  sys.puts("First list has multiple elements [1,2] + [3]");
+  var list1 = emptyList.cons(2).cons(1);
+  var list2 = emptyList.cons(3);
+  var sum = list1.add(list2);
+
+  (function() {
+    expect_equal(1, sum.head(), "first element is 1");
+  }());
+
+  (function() {
+    expect_equal(2, sum.tail().head(), "second element is 2");
+  }());
+
+  (function() {
+    expect_equal(3, sum.tail().tail().head(), "third element is 3");
+  }());
+}());
+
 
