06102096aadb9baeaf20340eff0b8f2bf135d0ea
coreyhaines <coreyhaines@gmail.com>
2010-05-20 18:42:20 +0200

    Single element list has head of same list

--- spec/ch2/list_suffixes_spec.js
+++ spec/ch2/list_suffixes_spec.js
@@ -6,6 +6,7 @@ var sys = require('sys'),
     expect_equal = require('../spec_helper').expect_equal,
     emptyList = lib_require('ch2/list').empty;
 
+sys.puts("Suffix");
 
 (function() {
     sys.puts("empty list");
@@ -13,3 +14,11 @@ var sys = require('sys'),
     expect(list.suffixes().head().isEmpty(), "empty list has list of the empty list");
 }());
 
+(function() {
+    sys.puts("single element");
+    var list = emptyList.cons('a');
+
+    (function() {
+      expect_equal(list, list.suffixes().head(), "head of list is the single-element list");
+    }());
+}());
