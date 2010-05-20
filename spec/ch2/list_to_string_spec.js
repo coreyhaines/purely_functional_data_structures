3c8aeb30d11b2690f0f9163dd1d27de2e4c6c9d1
coreyhaines <coreyhaines@gmail.com>
2010-05-20 18:57:10 +0200

    toString for empty list

---
+++ spec/ch2/list_to_string_spec.js
@@ -0,0 +1,15 @@
+var sys = require('sys'),
+    p = require('../spec_helper').p,
+    lib_require = require('../spec_helper').lib_require,
+    it = require('../spec_helper').it,
+    expect = require('../spec_helper').expect,
+    expect_equal = require('../spec_helper').expect_equal,
+    list = lib_require('ch2/list');
+
+sys.puts("toString");
+(function() {
+  sys.puts("Empty list");
+  expect_equal("[]", list.empty.toString(), "renders as []");
+}());
+
+
